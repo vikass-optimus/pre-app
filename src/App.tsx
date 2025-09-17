afbcdd2617465ae3e1ed08359a0b1912f24ee94f
vikassinghal20 <vsinghal@cendyn.com>
2025-09-17 12:56:58 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="mb-4">
+        Hello world!
+      </h1>
+      <p>Vikas Singhal</p>
+    </>
+  )
+}
+
+export default App
